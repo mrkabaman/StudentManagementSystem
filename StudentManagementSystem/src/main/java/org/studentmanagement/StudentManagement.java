cdf90faa43d6c4d087db3889b63f85632d48b20a
mrkabaman <mrkabaman@users.noreply.github.com>
2023-02-10 20:25:51 +0000

    Adding in assume logic for the krissy's student management system (#2)

--- StudentManagementSystem/src/main/java/org/studentmanagement/StudentManagement.java
+++ StudentManagementSystem/src/main/java/org/studentmanagement/StudentManagement.java
@@ -24,8 +24,61 @@ public class StudentManagement
 
             System.out.println("\n");
 
-            students[i] = new Student(firstName,lastName);
+            //Create the basic studentBeingProcess
+            Student studentBeingProcess = new Student(firstName,lastName);
+
+            System.out.print("Enter student class level:\n1. Freshmen\n2. Sophomore\n3. Junior\n4. Senior\n");
+            int gradeYear = in.nextInt();
+            studentBeingProcess.setGradeYear(gradeYear);
+            studentBeingProcess.setStudentID();
+
+            enroll(studentBeingProcess);
+            payTuition(studentBeingProcess);
+            viewBalance(studentBeingProcess);
+
+            students[i] = studentBeingProcess;
+
+            in.nextLine();
 
         }
+
+
     }
+
+    public static void enroll(Student student)
+    {
+        do {
+            System.out.print("Enter course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")){
+                student.setCourse(course);
+                int newTuitionBalance = student.getTuitionBalance() + student.getCostOfCourse();
+                student.setTuitionBalance(newTuitionBalance);
+            } else {
+                break;
+            }
+        } while (1 != 0);
+    }
+
+    public static void viewBalance(Student student)
+    {
+        System.out.println("Your balance is: $" + student.getTuitionBalance());
+    }
+
+    public static void payTuition(Student student)
+    {
+        viewBalance(student);
+
+        System.out.print("Enter your payment: $");
+        Scanner in = new Scanner(System.in);
+        int payment = in.nextInt();
+        int newTuitionBalance = (student.getTuitionBalance()) - payment;
+        student.setTuitionBalance(newTuitionBalance);
+        System.out.println("Thank you for your payment of $" + payment);
+
+
+        viewBalance(student);
+    }
+
 }
